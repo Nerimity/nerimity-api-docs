c9995f7b0f6a281f415168a78616b82ca558c6ee
Supertiger <saawalkhn@googlemail.com>
2025-05-14 10:46:05 +0100

    im so confused

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,7 +21,7 @@ const renameMe: () => PluginOption = () => ({
 
 export default defineConfig({
   plugins: [solid(), renameMe()],
-  base: "/",
+  base: "/nerimity-docs/",
   optimizeDeps: {
     include: ["solid-markdown > micromark", "solid-markdown > unified"],
   },
